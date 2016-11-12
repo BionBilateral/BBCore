edb9861a7643b3d0eb432de0be9d63b743fa854e
William Towe <willbur1984@gmail.com>
2016-11-12 16:58:58 -0500

    Initial Commit

---
+++ BBCore/BBCore.h
@@ -0,0 +1,19 @@
+//
+//  BBCore.h
+//  BBCore
+//
+//  Created by William Towe on 11/12/16.
+//  Copyright © 2016 Bion Bilateral, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BBCore.
+FOUNDATION_EXPORT double BBCoreVersionNumber;
+
+//! Project version string for BBCore.
+FOUNDATION_EXPORT const unsigned char BBCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BBCore/PublicHeader.h>
+
+
